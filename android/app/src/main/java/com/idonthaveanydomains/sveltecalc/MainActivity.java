c98547a3512fa133e4d3f3b6283127739f65b642
neurocrash59 <synthiv1916@gmail.com>
2021-12-10 13:43:39 +0700

    Attempting to add capcitor.js to build a mobile app with appcircle

---
+++ android/app/src/main/java/com/idonthaveanydomains/sveltecalc/MainActivity.java
@@ -0,0 +1,5 @@
+package com.idonthaveanydomains.sveltecalc;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/store.js
+++ src/store.js
@@ -1,18 +1,3 @@
 import { writable } from 'svelte/store';
 
 export const display = writable('');
-
-// function createDisplay() {
-//   const { subscribe, set, update } = writable(0);
-
-//   return {
-//     subscribe,
-//     add: (num) => update((n) => n + num),
-//     subtract: (num) => update((n) => n - num),
-//     multiply: (num) => update((n) => n * num),
-//     divide: (num) => update((n) => n / num),
-//     reset: () => set(0),
-//   };
-// }
-
-// export const display = createDisplay();
